ce417fd4243f0750b847f6cb4ed06a6361b52066
Akshay Dewan <akudewan@gmail.com>
2015-05-10 17:04:39 +0530

    Create notification properly. Reset previous distance when a new run is started

--- app/src/main/java/me/akshaydewan/endomartian/MartianService.java
+++ app/src/main/java/me/akshaydewan/endomartian/MartianService.java
@@ -12,10 +12,15 @@ public class MartianService extends Service {
   public static final String DISTANCE_EXTRA = "me.akshaydewan.endomartian.extras.distance";
   public static final String PACE_EXTRA = "me.akshaydewan.endomartian.extras.pace";
   public static final String PACE_UNIT_EXTRA = "me.akshaydewan.endomartian.extras.paceUnit";
+  public static final String RESET_EXTRA = "me.akshaydewan.endomartian.extras.reset";
   private float prevDistance = 0;
 
   @Override
   public int onStartCommand(Intent intent, int flags, int startId) {
+    boolean shouldReset = intent.getBooleanExtra(RESET_EXTRA, false);
+    if(shouldReset) {
+      reset();
+    }
     float distance = intent.getFloatExtra(DISTANCE_EXTRA, 0);
     String pace = intent.getStringExtra(PACE_EXTRA);
     String paceUnit = intent.getStringExtra(PACE_UNIT_EXTRA);
@@ -26,9 +31,15 @@ public class MartianService extends Service {
     return super.onStartCommand(intent, flags, startId);
   }
 
+  private void reset() {
+    this.prevDistance = 0;
+  }
+
   private void createNotification(String pace, String paceUnit) {
     NotificationCompat.Builder mBuilder =
         new NotificationCompat.Builder(this)
+            .setSmallIcon(R.mipmap.ic_launcher)
+            .setContentText(getString(R.string.pace))
             .setContentText(pace + " " + paceUnit);
     ((NotificationManager)getSystemService(NOTIFICATION_SERVICE)).notify(1, mBuilder.build());
   }
